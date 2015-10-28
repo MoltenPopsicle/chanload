92cca69266cc2da5693f85080b3ee906907029a7
MoltenPopsicle <mr.james.reggie@gmail.com>
2015-10-28 11:53:10 -0500

    Cleaned up code, added comments

--- chanload.py
+++ chanload.py
@@ -8,16 +8,16 @@ class imgdownload(object):
     def __init__(self):
         return
     def imginsert(self):
-        headers = { 'User-Agent' : 'Mozilla/5.0' }
-        agent = urllib2.Request(thread_url, None, headers)
+        headers = { 'User-Agent' : 'Mozilla/5.0' } 
+        agent = urllib2.Request(thread_url, None, headers) #Sets the script's user agent header to Mozilla/5.0, as 8chan blcoks requests from python user agents
         tfile = urllib2.urlopen(agent)
-        tparse = re.findall(r'href="(/[a-z]+/src/+\d+\.(jpg|jpeg|png|webm|mp4|gif|pdf))"', str(tfile.read()))
+        tparse = re.findall(r'href="(/[a-z]+/src/+\d+\.(jpg|jpeg|png|webm|mp4|gif|pdf))"', str(tfile.read())) #regex to find images
 
         global imglist
         imglist = []
         for i in tparse:
             imglist.append(i[0])
-        imglist =  self.unique(imglist)
+        imglist =  self.unique(imglist) #Removes duplicate file entries by invoking unique checker below
      
         print("Found "+str(len(imglist))+" in the thread")
 
@@ -36,22 +36,21 @@ class imgdownload(object):
 
         if not os.path.exists(folder):
             os.makedirs(folder)
-            os.chdir(folder)
+            os.chdir(folder) #Checks if folder already exists. If not, creates it.
         else:
             os.chdir(folder)
         print("Making folder")
  
-        fileno = 0
-        while fileno < len(imglist):
+        fileno = 0 
+        while fileno < len(imglist): #Uses a number increasing by 1 to count the current index entry in the list
             img_file = 'https://8ch.net' + imglist[fileno]
-            imagelist = str(tuple(imglist))
-            filename = ''.join(re.findall(r'\d+', str(imglist[fileno])))
+            filename = ''.join(re.findall(r'\d+', str(imglist[fileno]))) #Uses file counter and sets filename as the original filename's number of current list entry
 
             response = requests.get(img_file, stream=True)
        
             with open(filename, 'wb') as f:  
-                response.raw.decode_content = True
-                shutil.copyfileobj(response.raw, f)
+                response.raw.decode_content = True 
+                shutil.copyfileobj(response.raw, f)  #Copies raw data gotten from requests into a filee
             print("Downloading image "+str(fileno + 1)+" of the thread")
             fileno += 1
        
